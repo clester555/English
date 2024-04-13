daaa47e9dff3f21d52ae7b9e0f5e989bf4d3ef67
clest46 <clest46@wgu.edu>
2024-04-14 00:04:41 +0800

    getting close

---
+++ js/objects/Word.js
@@ -0,0 +1,15 @@
+class Word{
+    constructor(text){
+        this.image = null;
+        this.audio = null;
+        this.text = text;  
+    }
+
+    setImage(image){
+        this.image = image;
+    }
+
+    setAudio(audio){
+        this.audio = audio;
+    }
+}
--- js/scenes/GameScene.js
+++ js/scenes/GameScene.js
@@ -7,48 +7,62 @@ class GameScene extends Phaser.Scene{
     }
 
     preload(){
-        this.load.atlas('flares', 'assets/particles/flares.png', 'assets/particles/flares.json');
-        this.shapes = ["circle","dot","heart","line","oval","rectangle","square","star","triangle"]
-        this.load.audio('explosionAudio','assets/sounds/explode.mp3');
-        for(let i = 0; i<this.shapes.length;i++){
-            this.load.image(this.shapes[i]+"Image", "assets/images/"+this.shapes[i]+".png");
-            this.load.audio(this.shapes[i]+"Audio",["assets/sounds/"+this.shapes[i]+".mp3",
-                                                    "assets/sounds/"+this.shapes[i]+".ogg"]);
-        }
+        let wordList = ["circle","dot","heart","line","oval","rectangle","square","star","triangle"]
+        let secretWordList = ["Drum","Rabbit"];
+        this.words = [];
+        this.secretWords = [];
         
+        for(let i = 0; i<wordList.length;i++){
+            this.words[i] = new Word(wordList[i]);
+            this.words[i].setImage(this.load.image(wordList[i]+"Image", "assets/images/"+wordList[i]+".png"));
+            this.words[i].setAudio(this.load.audio(wordList[i]+"Audio", ["assets/sounds/"+wordList[i]+".mp3",
+                                                    "assets/sounds/"+wordList[i]+".ogg"]));
+        }
+
+        for(let i = 0; i<secretWordList.length;i++){
+            this.secretWords[i] = new Word(secretWordList[i]);
+            //this.word[i].setImage(this.load.image(this.shapes[i]+"Image", "assets/images/"+this.words[i]+".png"));
+            //this.word[i].setAudio(this.load.audio(this.shapes[i]+"Audio",["assets/sounds/"+this.shapes[i]+".mp3",
+            //                                        "assets/sounds/"+this.shapes[i]+".ogg"]));
+        }
+        this.load.atlas('flares', 'assets/particles/flares.png', 'assets/particles/flares.json');
+        this.load.audio('explosionAudio','assets/sounds/explode.mp3');
+       
     }
 
     create(){
+        this.secretLetters = [];
+        this.buttons = [];
         this.sounds = [];
-        for(let i = 0; i<this.shapes.length;i++){
-             this.sounds[i] = this.sound.add(this.shapes[i]+"Audio");
+        this.prompts = [];
+        this.promptIndex = 0;
 
-        }
         this.explosionAudio = this.sound.add('explosionAudio',{volume:0.1});
-        this.explosionAudio.setSeek(1);
 
         this.makeClock();
         this.makeStartButton();
-        this.makeButtons();
         this.makePrompt();
-        
         
         this.input.on('gameobjectdown', (pointer, gameObject) =>
         {
-            
-            if(gameObject.name == this.shapes[this.answer]){
+            console.log(this.prompts[this.promptIndex].text);
+            if(gameObject.name == this.prompts[this.promptIndex].text){
                 emitter.x =gameObject.x;
                 emitter.y =gameObject.y;
+               for(let i = 0;i<this.buttons.length;i++){
+                    if (gameObject.name==this.buttons[i].name){
+                        this.secretLetters[i].setVisible(true);
+                    }
+                }
                 gameObject.destroy();
                 this.explosionAudio.play();
                 emitter.explode(16);
+                this.promptIndex+=1;
                 this.makeNewPrompt();
             } else{
                 
             }
             
-            
-
         });
        
         this.timedEvent = this.time.addEvent({ delay: 100, callback: this.onEvent, callbackScope: this,loop:true });
@@ -78,7 +92,7 @@ class GameScene extends Phaser.Scene{
                 this.seconds -= 1;
             }
             this.countDownClock.text = this.seconds + "." + this.tenthsOfSecond;
-            this.randomTints(this.buttons[Phaser.Math.Between(0,8)]);
+            //this.randomTints(this.buttons[Phaser.Math.Between(0,8)]);
         }
     }
 
@@ -91,28 +105,7 @@ class GameScene extends Phaser.Scene{
         this.tenthsOfSecond =0;
         this.seconds = 60;
         this.isPlaying = true;
-
-        for(let i = 0; i<this.shapes.length;i++){
-            this.buttons[i].setVisible(true);
-        }
-        
-        this.makeNewPrompt();
-        
-    }
-    
-    makeButtons(){
-        this.buttons = [];
-        this.wrongButtons = [];
-        for(let i = 0; i<this.shapes.length;i++){
-            this.buttons[i] = this.add.sprite(i*100,0,this.shapes[i]+"Image");
-            this.buttons[i].setName(this.shapes[i]);
-            this.buttons[i].setInteractive();
-            this.buttons[i].setScale(0.5,0.5);
-            this.buttons[i].setPosition(i*64+150,300);
-            this.buttons[i].setVisible(false);
-            this.randomTints(this.buttons[i]);
-        }
-   
+        this.makeSecretWord();
     }
     
     makeStartButton(){
@@ -150,6 +143,7 @@ class GameScene extends Phaser.Scene{
     }
 
     makePrompt(){
+        
         this.prompt = this.add.text(400, 100, '?', {
             fontFamily: 'Arial',
             fontSize: '64px',
@@ -161,13 +155,9 @@ class GameScene extends Phaser.Scene{
     }
 
     makeNewPrompt(){
-        let old = this.answer;
-        while (old == this.answer){
-            this.answer = Math.floor(Math.random() * this.shapes.length);
-        }
-        this.prompt.text = this.shapes[this.answer];
-        this.sounds[this.answer].play();
-
+        console.log(this.prompts[0]);
+        this.prompt.text = this.prompts[this.promptIndex].text;
+        this.sounds[this.promptIndex].play();
     }
 
     randomTints(gameObject){
@@ -179,6 +169,66 @@ class GameScene extends Phaser.Scene{
         let g = Phaser.Math.Between(0,255);
         let b = Phaser.Math.Between(0,255);
         return Phaser.Display.Color.GetColor(r,g,b);
+    }
+
+    makeSecretWord(){
+        this.secertWord = this.secretWords[Phaser.Math.Between(0,this.secretWords.length-1)];
+        let wordList = [];
+        for(let i = 0;i<this.words.length;i++){
+            wordList[i] = i;
+        }
+        for(let i = 0;i<this.words.length;i++){
+            let a = Phaser.Math.Between(0,wordList.length-1);
+            while(a==i){
+                a = Phaser.Math.Between(0,wordList.length-1);
+            }
+            let temp = wordList[i];
+            wordList[i]=wordList[a];
+            wordList[a] = temp;
+        }
+
+        let size = 80*this.secertWord.text.length;
+        let start = 440-size/2;
+
+        for(let i = 0;i<this.secertWord.text.length;i++){
+            this.secretLetters[i] = this.add.text(start+80*i, 250, this.secertWord.text[i], {
+                fontFamily: 'Arial',
+                fontSize: '80px',
+                color: '#ff11ff',
+                align: 'center',
+                fixedWidth: 400
+            }).setPadding(32).setOrigin(0.5);
+            this.secretLetters[i].setVisible(false);
+            
+            this.buttons[i] = this.add.image(i*100,0,this.words[wordList[i]].text+"Image");
+            this.buttons[i].setName(this.words[wordList[i]].text);
+            this.buttons[i].setInteractive();
+            this.buttons[i].setScale(0.6,0.6);
+            this.buttons[i].setPosition(start+80*i,250);
+            this.randomTints(this.buttons[i]);
+        }
+     
+        for(let i = 0;i<this.secertWord.text.length;i++){
+            this.prompts[i] = this.words[wordList[i]];
+        }
+
+        
+        for(let i = 0;i<this.prompts.length;i++){
+            let a = Phaser.Math.Between(0,this.prompts.length-1);
+            while(a==i){
+                a = Phaser.Math.Between(0,this.prompts.length-1);
+            }
+            let temp = this.prompts[i];
+            this.prompts[i]=this.prompts[a];
+            this.prompts[a] = temp;
+        }
+       
+        this.promptIndex = 0;
+
+        for(let i = 0;i<this.prompts.length;i++){
+            this.sounds[i] = this.sound.add(this.prompts[i].text+"Audio");  
+        }
+        this.makeNewPrompt();
     }
 
 
